c552054a8066b017a3925d355d94ed8b54a3023f
Porter Hatch <porterhatch1@gmail.com>
2023-04-25 18:35:38 -0600

    Initial Commit

---
+++ NutritionTracker/NutritionTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  NutritionTrackerApp.swift
+//  NutritionTracker
+//
+//  Created by Porter Hatch on 4/25/23.
+//
+
+import SwiftUI
+
+@main
+struct NutritionTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
